bcfac0d0e65768552cf28ffcc83a7a6106f31f1c
nonepage <chendaxians@outlook.com>
2020-05-24 12:24:56 +0800

    稳定版本，不操作用户注册表，可搭配任务计划使用

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ import math
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import pad
 from lxml import etree
-import pywintypes
-import win32api
-import win32con
+
 
 
 # 模拟前端CryptoJS加密
@@ -242,23 +240,9 @@ def submitForm(formWid, address, collectWid, schoolTaskWid, form, cookies):
     msg = r.json()['message']
     return msg
 
-def register():
-    name = 'nonepage_dalay'  # 要添加的项值名称
-    program_path = os.path.abspath('.') + '\\main.exe'  # 要添加的exe路径
-    # 注册表项名
-    KeyName = 'Software\\Microsoft\\Windows\\CurrentVersion\\Run'
-    # 异常处理
-    try:
-        key = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, KeyName, 0, win32con.KEY_ALL_ACCESS)
-        win32api.RegSetValueEx(key, name, 0, win32con.REG_SZ, program_path)
-        win32api.RegCloseKey(key)
-    except:
-        print('添加失败')
-    print('添加成功！')
+
 def main():
     config = getConfig()
-    register()
-
     while True:
         log('脚本开始执行。。。')
         cookies = getCookies(config)
